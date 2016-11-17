b02010d82406e0c1a2bb52d3aafe0b780794d28b
remi-h1 <remito@live.fr>
2016-11-17 10:13:45 +0100

    Create v_entete.php

---
+++ v_entete.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!-- TITRE ET MENUS -->
+<html lang="fr">
+    <head>
+        <title><?php echo $titre; ?></title>
+        <meta http-equiv="Content-Language" content="fr">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link href="util/cssGeneral.css" rel="stylesheet" type="text/css">
+    </head>
+    <body >
+
+    <div class="header">
+        <h1><?php echo $$titre; ?></h1>
