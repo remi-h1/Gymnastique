885a36fadf621609c21b4331212cb75a35b75fe0
remi-h1 <remito@live.fr>
2016-11-17 08:43:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<?php
+session_start();
+require_once("util/fonctions.inc.php");
+require_once("util/class.pdoChampionnatGym.inc.php");
+include("vues/v_entete.php") ;
+include("vues/v_bandeau.php") ;
 
+if(!isset($_REQUEST['uc']))
+     $uc = 'accueil';
+else
+	$uc = $_REQUEST['uc'];
+
+$pdo = PdoChampionnatGym::getPdoChampionnatGym();	 
+switch($uc)
+{
+	case 'accueil':
+		{include("vues/v_accueil.php");break;}
+}
+include("vues/v_pied.php") ;
+?>
