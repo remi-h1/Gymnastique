bd7d94f06348f2b08657625af11eb0b3e903f553
MDSIO <maxime.dupont49280@gmail.com>
2016-11-17 10:08:06 +0100

    Create bandeau.php

---
+++ bandeau.php
@@ -0,0 +1,8 @@
+<ul>
+	<li><a href="index.php?uc=accueil"> Accueil </a></li>
+	<li><a href="index.php?uc=gererJuge&action=voirJuges"> Voir les Juges</a></li>
+	<li><a href="index.php?uc=gererJuge&action=voirUnJuge"> Voir un Juge </a></li>
+	<li><a href="index.php?uc=gererJuge&action=modifierJuge"> Modifier un Juge </a></li>
+	<li><a href="index.php?uc=gererJuge&action=supprimerJuge"> Supprimer un Juge </a></li>
+	<li><a href="index.php?uc=affecterJuges"> Affecter un Juge à un hébergement</a></li>
+</ul>
