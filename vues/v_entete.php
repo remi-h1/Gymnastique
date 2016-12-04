fcba4d085a8c33788ce360ee2d797cbf656252e9
remi-h1 <remito@live.fr>
2016-12-04 23:26:06 +0100

    Update v_entete.php

--- vues/v_entete.php
+++ vues/v_entete.php
@@ -1,3 +1,5 @@
+<!--  auteur : Rémi Hillériteau -->
+
 <!DOCTYPE html>
 <!-- TITRE ET MENUS -->
 <html lang="fr">
@@ -5,8 +7,11 @@
         <title><?php echo $titre; ?></title>
         <meta http-equiv="Content-Language" content="fr">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width" /> <!-- pour un site en design responsive -->
         <link href="util/cssGeneral.css" rel="stylesheet" type="text/css">
+        <link href="util/bandeau.css" rel="stylesheet" type="text/css">
     </head>
+    
     <body >
 
     <div class="header">
