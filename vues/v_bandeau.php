777a91cb8af2606f149c8531d91e4e076e48c3b5
remi-h1 <remito@live.fr>
2016-12-04 23:25:17 +0100

    Update v_bandeau.php

--- vues/v_bandeau.php
+++ vues/v_bandeau.php
@@ -1,8 +1,62 @@
-<ul>
-	<li><a href="index.php?uc=accueil"> Accueil </a></li>
-	<li><a href="index.php?uc=gererJuge&action=voirJuges"> Voir les Juges</a></li>
-	<li><a href="index.php?uc=gererJuge&action=voirUnJuge"> Voir un Juge </a></li>
-	<li><a href="index.php?uc=gererJuge&action=modifierJuge"> Modifier un Juge </a></li>
-	<li><a href="index.php?uc=gererJuge&action=supprimerJuge"> Supprimer un Juge </a></li>
-	<li><a href="index.php?uc=affecterJuges"> Affecter un Juge à un hébergement</a></li>
-</ul>
+<div id="menu">
+	<dl>
+		<dt>Hébergements</dt>
+			<dd>
+				<ul>
+					<a href="index.php?uc=gererHebergementJuges&action=afficherListeHebergement"><li>Voir les Hébergements</li></a>
+					<a href="index.php?uc=gererHebergementJuges&action=nouvelHebergement"><li>Ajouter un Hébergement</li></a>		
+				</ul>	
+			</dd>
+	</dl>
+
+	<dl>
+		<dt>Juges</dt>
+			<dd>
+				<ul>
+					<a href="index.php?uc=gererJuge&action=voirJuges"><li> Voir les Juges</li></a>
+					<a href="index.php?uc=gererJuge&action=ajouterUnJuge"><li> Ajouter un Juge</li></a>				
+					<a href="index.php?uc=gererJuge"><li>Affecter un Juge à un hébergement</li></a>			
+				</ul>	
+			</dd>
+	</dl>
+
+	<dl>
+		<dt>Prestations</dt>
+			<dd>
+				<ul>
+					<a href="index.php?uc=nombreParticipants&action=voirJuges"><li>Choix des prestations</li></a>			
+				</ul>	
+			</dd>
+	</dl>
+
+	<dl>
+		<dt>Club</dt>
+			<dd>
+				<ul>
+					<a href="index.php?uc=gererPartieComptable&action=voirJuges"><li>Nombre de participants</li></a>			
+				</ul>	
+			</dd>
+	</dl>
+
+	<dl>
+		<dt>Repas</dt>
+			<dd>
+				<ul>
+					<a href="index.php?uc=gererCommissionRestauration&action=voirJuges"><li>Voir le nombre de personne pour un repas</li></a>
+				</ul>	
+			</dd>
+	</dl>
+
+	<dl>
+		<dt>Paiement</dt>
+			<dd>
+				<ul>
+					<a href="index.php?uc=clubChoisirPrestation&action=voirJuges"><li>Voir les Paiements</li></a>
+					<a href="index.php?uc=clubChoisirPrestation&action=voirUnJuge"><li>Ajouter un Paiement </li></a>				
+				</ul>	
+			</dd>
+	</dl>
+</div>
+
+</div> <!-- On ferrme le <div classe='header'> -->
+<div class='page'>
