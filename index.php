870bc7f751b6280d40252b09ff080e7971b8be9a
remi-h1 <remito@live.fr>
2016-12-06 23:23:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+// le controleur principal
+// auteur : Rémi Hillériteau
 session_start();
 require_once("util/fonctions_PHP.php");
 require_once("util/class.pdoChampionnatGym.php");
-
 if(!isset($_REQUEST['uc']))
 	$uc = 'accueil';
 else
@@ -10,13 +11,15 @@ else
 
 include("controleurs/c_entete.php") ;
 include("vues/v_bandeau.php") ;
-
 $pdo = PdoChampionnatGym::getPdoChampionnatGym();
-
 switch($uc)
 {
 	case 'accueil':
 		{include("vues/v_accueil.php");break;}
+
+	case 'gererHebergementJuges':
+		{include("controleurs/c_gererHebergementJuges.php");break;}
 }
 include("vues/v_pied.php") ;
+require_once("util/fonctions_JS.php");
 ?>
