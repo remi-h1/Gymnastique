1e03aee9dd210b4197f6b2d1f94ce1c7f7afcf3e
remi-h1 <remito@live.fr>
2016-12-06 23:22:23 +0100

    Delete v_unHebergement.php

--- vues/v_unHebergement.php
+++
@@ -1,7 +0,0 @@
-<!--  auteur : Rémi Hillériteau -->
-<h2><?php echo $nom; ?></h2>
-<span><?php echo $adresse . " " . $cp . " " . $ville; ?></span><br/>
-<span><?php echo $type; ?></span><br/>
-<span>tel : <?php echo $tel; ?></span><br/>
-<span>mail : <?php echo $mail; ?></span><br/>
-<!-- plus d'infos à venir -->
